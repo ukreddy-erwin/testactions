2604e3f08078596190b243dfb4056f4f317278a6
Uday Kiran Reddy <ukreddy@erwin.com>
2022-06-02 11:20:14 +0530

    Update goodbye.sh

--- goodbye.sh
+++ goodbye.sh
@@ -1,7 +1,3 @@
 echo "Goodbye"
-export PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin"
-printenv
-echo $(inputs.who-to-greet)
-echo $inputs.who-to-greet
-echo ${{ inputs.who-to-greet }}
-echo ${{ inputs.userfulname }}
+echo $who-to-greet
+echo $userfulname
